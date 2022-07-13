2794a2e9596eb3416b1d48a8bc1775b822f4c701
iRandell <irandellhart@gmail.com>
2022-07-13 20:21:02 +0300

    fix: change EventPool methods return types

--- src/event-pool.ts
+++ src/event-pool.ts
@@ -3,8 +3,8 @@ import type { Callback } from './callback'
 export interface EventPool<TArgs extends unknown[]> {
   readonly size: number
 
-  on(handler: Callback<TArgs>): Callback<TArgs>
-  once(handler: Callback<TArgs>): Callback<TArgs>
+  on(handler: Callback<TArgs>): () => void
+  once(handler: Callback<TArgs>): () => void
   off(handler: Callback<TArgs>): void
   offAll(): void
 }
