7ebb81f20faa9399176d9c82620801be3dc8cd65
iRandell <irandellhart@gmail.com>
2022-07-14 20:05:51 +0300

    refactor: add maxCallCount to EventPool

--- src/event-pool.ts
+++ src/event-pool.ts
@@ -3,7 +3,7 @@ import type { Callback } from './callback'
 export interface EventPool<TArgs extends unknown[]> {
   readonly size: number
 
-  on(handler: Callback<TArgs>): () => void
+  on(handler: Callback<TArgs>, maxCallCount?: number): () => void
   once(handler: Callback<TArgs>): () => void
   off(handler: Callback<TArgs>): void
   offAll(): void
